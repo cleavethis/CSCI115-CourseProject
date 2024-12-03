6239b20d7e0b4caf86a8d9dceeb579124dfe15f4
Rick <111731365+cleavethis@users.noreply.github.com>
2024-12-03 13:45:47 -0800

    Add project files.

---
+++ CSCI115-CourseProject/OrderManager.h
@@ -0,0 +1,22 @@
+#ifndef ORDERMANAGER_H
+#define ORDERMANAGER_H
+#include <iostream>
+using namespace std;
+
+
+
+class OrderManager
+{
+	struct Order {
+
+		string orderID;
+		int orderPriority;
+		string orderDestination
+
+	};
+
+};
+
+
+#endif ORDERMANAGER_H
+
---
+++ CSCI115-CourseProject/main.cpp
@@ -0,0 +1,12 @@
+// Rick Richmond
+#include "OrderManager.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	// code will go here
+
+	return 0;
+}
